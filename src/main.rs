f254c5d8d58d30a921c1e254e1856d23a8a81c36
zyenapz <zyenapz@gmail.com>
2022-07-11 22:51:05 +0800

    added ability to shoot

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,30 @@
 /// Station commander!
 /// Defend your space station under attack!
 /// TODO:
-/// 1. add a way to shoot
-/// 2. add function for spawning fighters
-/// 3. add simple enemies
-use bevy::prelude::*;
+/// + add a way to shoot
+/// - add a way to aim
+/// - add cooldown for shooting
+/// - add function for spawning fighters
+/// - add simple enemies
+/// - add a way to despawn the bullets when they're out of view
+use bevy::{
+    ecs::system::QuerySingleError, prelude::*, reflect::erased_serde::private::serde::__private::de,
+};
 
 #[derive(Component)]
 struct Player;
+
+#[derive(Component)]
+struct Bullet {
+    direction: Vec2,
+}
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_startup_system(setup)
-        .add_system(move_player)
+        .add_system(move_bullet)
+        .add_system(shoot)
         .insert_resource(ClearColor(Color::rgb(0., 0.4, 0.4)))
         .run();
 }
@@ -42,32 +53,33 @@ fn setup(mut commands: Commands) {
         .insert(Player);
 }
 
-const PLAYER_SPEED: f32 = 350.;
-fn move_player(
-    kb: Res<Input<KeyCode>>,
-    time: Res<Time>,
-    mut query: Query<(&Player, &mut Transform)>,
-) {
-    let mut transform = query.single_mut().1;
-
-    // println!("{} {}", transform.translation.x, transform.translation.y);
-
-    let mut dx = 0.;
-    let mut dy = 0.;
-
-    if kb.pressed(KeyCode::W) {
-        dy = PLAYER_SPEED * time.delta_seconds();
+fn shoot(mut commands: Commands, kb: Res<Input<KeyCode>>) {
+    if kb.pressed(KeyCode::Space) {
+        commands
+            .spawn_bundle(SpriteBundle {
+                sprite: Sprite {
+                    color: Color::rgb(1., 0., 0.),
+                    custom_size: Some(Vec2::new(10., 10.)),
+                    ..default()
+                },
+                transform: Transform::from_xyz(100., 0., 0.),
+                ..default()
+            })
+            .insert(Bullet {
+                direction: Vec2::new(0., 1.),
+            });
     }
-    if kb.pressed(KeyCode::S) {
-        dy = PLAYER_SPEED * -time.delta_seconds();
-    }
-    if kb.pressed(KeyCode::A) {
-        dx = PLAYER_SPEED * -time.delta_seconds();
-    }
-    if kb.pressed(KeyCode::D) {
-        dx = PLAYER_SPEED * time.delta_seconds();
-    }
+}
 
-    transform.translation.x += dx;
-    transform.translation.y += dy;
+const BULLET_SPEED: f32 = 550.;
+fn move_bullet(time: Res<Time>, mut query: Query<(&Bullet, &mut Transform)>) {
+    for (b, mut tr) in query.iter_mut() {
+        let mut direction = b.direction;
+
+        let mut dx = BULLET_SPEED * time.delta_seconds();
+        let mut dy = BULLET_SPEED * time.delta_seconds();
+
+        tr.translation.x += dx;
+        tr.translation.y += dy;
+    }
 }
