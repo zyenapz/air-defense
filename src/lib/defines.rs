163586f687c178ad74e6207374b3135f7989a045
zyenapz <zyenapz@gmail.com>
2022-07-28 14:57:17 +0800

    organized player code into player.rs

--- src/lib/defines.rs
+++ src/lib/defines.rs
@@ -3,5 +3,7 @@ pub const WND_RES: (f32, f32) = (800., 800.);
 pub const WND_TITLE: &str = "Station Commander";
 
 // --- PLAYER DEFINES ---
-pub const P_INIT_HEALTH: f64 = 10.;
-pub const P_INIT_ARMOR: f64 = 0.;
+pub const P_INIT_HEALTH: f32 = 10.;
+pub const P_INIT_ARMOR: f32 = 0.;
+pub const PB_INIT_SPEED: f32 = 300.;
+pub const P_INIT_FIRE_COOLDOWN: u64 = 100;
--- src/lib/player.rs
+++ src/lib/player.rs
@@ -1,14 +1,40 @@
-use bevy::{prelude::*, render::color};
+use std::time::Duration;
 
-use super::defines::{P_INIT_ARMOR, P_INIT_HEALTH};
+use bevy::prelude::*;
+
+use crate::lib::defines::PB_INIT_SPEED;
+
+use super::defines::{P_INIT_ARMOR, P_INIT_FIRE_COOLDOWN, P_INIT_HEALTH};
 
 #[derive(Component)]
-struct Player {
-    health: f64,
-    armor: f64,
+pub struct Station {
+    health: f32,
+    armor: f32,
+}
+
+#[derive(Component)]
+pub struct Gun {
+    rotation: f32,
+}
+
+#[derive(Component)]
+pub struct Bullet {
+    speed: f32,
+    direction: Vec2,
+}
+
+#[derive(Component)]
+pub struct FireCooldown {
+    timer: Timer,
 }
 
 pub fn setup_player(mut commands: Commands) {
+    let station = Station {
+        health: P_INIT_HEALTH,
+        armor: P_INIT_ARMOR,
+    };
+    let gun = Gun { rotation: 0. };
+
     commands
         .spawn_bundle(SpriteBundle {
             sprite: Sprite {
@@ -19,8 +45,66 @@ pub fn setup_player(mut commands: Commands) {
             transform: Transform::from_xyz(0., 0., 0.),
             ..default()
         })
-        .insert(Player {
-            health: P_INIT_HEALTH,
-            armor: P_INIT_ARMOR,
-        });
+        .insert(station)
+        .insert(gun);
+}
+
+pub fn setup_shoot_timer(mut commands: Commands) {
+    commands.insert_resource(FireCooldown {
+        timer: Timer::new(Duration::from_millis(P_INIT_FIRE_COOLDOWN), true),
+    })
+}
+
+pub fn control_player(
+    keyboard: Res<Input<KeyCode>>,
+    time: Res<Time>,
+    mut cooldown: ResMut<FireCooldown>,
+    mut commands: Commands,
+    mut query: Query<(&Station, &mut Gun)>,
+) {
+    // Update timer
+    cooldown.timer.tick(time.delta());
+
+    match query.get_single_mut() {
+        Ok(mut player) => {
+            // --- Rotate gun ---
+            if keyboard.pressed(KeyCode::Q) {
+                player.1.rotation -= 1.;
+            } else if keyboard.pressed(KeyCode::E) {
+                player.1.rotation += 1.;
+            }
+
+            // --- Fire bullet ---
+            if keyboard.pressed(KeyCode::Z) && cooldown.timer.finished() {
+                let angle = player.1.rotation.to_radians();
+                let dx = angle.sin();
+                let dy = angle.cos();
+
+                let bullet = Bullet {
+                    speed: PB_INIT_SPEED,
+                    direction: Vec2::new(dx, dy),
+                };
+
+                commands
+                    .spawn_bundle(SpriteBundle {
+                        sprite: Sprite {
+                            color: Color::YELLOW,
+                            custom_size: Some(Vec2::new(10., 10.)),
+                            ..default()
+                        },
+                        transform: Transform::from_xyz(0., 0., 1.),
+                        ..default()
+                    })
+                    .insert(bullet);
+            }
+        }
+        Err(e) => panic!("{}", e),
+    }
+}
+
+pub fn update_bullet(time: Res<Time>, mut query: Query<(&Bullet, &mut Transform)>) {
+    for (b, mut t) in query.iter_mut() {
+        t.translation.x += b.direction.x * b.speed * time.delta_seconds();
+        t.translation.y += b.direction.y * b.speed * time.delta_seconds();
+    }
 }
